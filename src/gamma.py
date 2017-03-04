022f3568956a2ef1152d99d9cbc8f2a540ed87ec
moz <moz@local>
2017-03-04 16:15:32 +0100

    login/logout working

--- src/gamma.py
+++ src/gamma.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
 app = Flask(__name__)
 
+def check_credentials( user, passwd ):
+    return True
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if not check_credentials( request.form['username'], request.form['password'] ):
+            error = 'Permission denied, credentials rejected'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
     app.run()
