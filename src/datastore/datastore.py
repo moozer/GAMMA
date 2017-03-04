ed5f4a851b86c29cc591f0b3480cbb2a435492ef
moz <moz@local>
2017-03-05 00:34:17 +0100

    initializing database test run

--- src/datastore/datastore.py
+++ src/datastore/datastore.py
@@ -1,6 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///:memory:', echo=False)
+
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String
 
 class Student( Base ):
     __tablename__ = 'students'
@@ -8,3 +12,9 @@ class Student( Base ):
     id = Column(Integer, primary_key=True)
     student_id = Column(String )
     name = Column(String)
+
+Base.metadata.create_all(engine)
+
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
--- src/tests/test_datastore.py
+++ src/tests/test_datastore.py
@@ -9,11 +9,20 @@ student_id = "abcd1234"
 student_name = "John Doe"
 
 class test_datastore(unittest.TestCase):
-    def testInit(self):
+    def testStudentMap(self):
         u = Student( student_id = student_id, name=student_name)
 
         self.assertEqual( u.student_id, student_id )
         self.assertEqual( u.name, student_name )
+
+    def testAddStudent( self ):
+        u = Student( student_id = student_id, name=student_name)
+
+        test_session = Session()
+        test_session.add( u)
+        test_session.commit()
+
+        self.assertTrue( u.id)
 
 if __name__ == '__main__':
     unittest.main()
