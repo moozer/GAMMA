a0f595680ed44b0ef3c299ae4883bfe2a82dde21
moz <moz@local>
2017-03-22 15:00:37 +0100

    finalized session -> lesson rename implemented extra_points in overview

--- src/datastore/__init__.py
+++ src/datastore/__init__.py
@@ -1,1 +1,7 @@
 # currently empty
+from datastore import Datastore
+
+from student import student_record
+from lesson import lesson_record
+from lesson_points import lesson_points_record
+from extra_points import extra_points_record
--- src/gamma.py
+++ src/gamma.py
@@ -1,12 +1,9 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
-from datastore.datastore import Datastore
-from datastore.student import student_record
-from datastore.session import session_record
-from datastore.session_points import session_points_record
-
 from datetime import date, datetime
+from datastore import *
+
 
 app = Flask(__name__)
 ds = Datastore()
@@ -19,22 +16,22 @@ def get_points_per_user( userid ):
     return {'absence': 1, 'attendance': 10, 'handins': 6, 'extra': 5, 'sum': 20 }
 
 def get_points_list_per_user( userid ):
-    absence =   [   { "date": "20170302", "session": "ITT1 SD" }]
-    attendance = [  { "date": "20170302", "session": "ITT1 SD", "sessionid": 23 },
-                    { "date": "20170102", "session": "ITT1 SD", "sessionid": 24 },
-                    { "date": "20170109", "session": "ITT1 SD", "sessionid": 25 },
-                    { "date": "20170116", "session": "ITT1 SD", "sessionid": 26 },
-                    { "date": "20170123", "session": "ITT1 SD", "sessionid": 27 },
-                    { "date": "20170130", "session": "ITT1 SD", "sessionid": 28 },
-                    { "date": "20170207", "session": "ITT1 SD", "sessionid": 29 },
-                    { "date": "20170214", "session": "ITT1 SD", "sessionid": 20 },
-                    { "date": "20170221", "session": "ITT1 SD", "sessionid": 21 } ]
-    handins = [     { "date": "20170302", "session": "ITT1 SD", "sessionid": 23 },
-                    { "date": "20170102", "session": "ITT1 SD", "sessionid": 24 },
-                    { "date": "20170109", "session": "ITT1 SD", "sessionid": 25 },
-                    { "date": "20170116", "session": "ITT1 SD", "sessionid": 26 },
-                    { "date": "20170123", "session": "ITT1 SD", "sessionid": 27 },
-                    { "date": "20170130", "session": "ITT1 SD", "sessionid": 28 } ]
+    absence =   [   { "date": "20170302", "lesson": "ITT1 SD" }]
+    attendance = [  { "date": "20170302", "lesson": "ITT1 SD", "lessonid": 23 },
+                    { "date": "20170102", "lesson": "ITT1 SD", "lessonid": 24 },
+                    { "date": "20170109", "lesson": "ITT1 SD", "lessonid": 25 },
+                    { "date": "20170116", "lesson": "ITT1 SD", "lessonid": 26 },
+                    { "date": "20170123", "lesson": "ITT1 SD", "lessonid": 27 },
+                    { "date": "20170130", "lesson": "ITT1 SD", "lessonid": 28 },
+                    { "date": "20170207", "lesson": "ITT1 SD", "lessonid": 29 },
+                    { "date": "20170214", "lesson": "ITT1 SD", "lessonid": 20 },
+                    { "date": "20170221", "lesson": "ITT1 SD", "lessonid": 21 } ]
+    handins = [     { "date": "20170302", "lesson": "ITT1 SD", "lessonid": 23 },
+                    { "date": "20170102", "lesson": "ITT1 SD", "lessonid": 24 },
+                    { "date": "20170109", "lesson": "ITT1 SD", "lessonid": 25 },
+                    { "date": "20170116", "lesson": "ITT1 SD", "lessonid": 26 },
+                    { "date": "20170123", "lesson": "ITT1 SD", "lessonid": 27 },
+                    { "date": "20170130", "lesson": "ITT1 SD", "lessonid": 28 } ]
     extra = [       { "date": "20170202",
                         "reason": "Awarded by class because of awesomeness", "points": 2},
                     { "date": "20170202",
@@ -85,40 +82,57 @@ def points( studentid=None ):
         return render_template('points_overview.html', points_list=points_list)
     else:
         name = ds.get_student(studentid).name
-        points_list = get_points_list_per_user( studentid )
+        lesson_points_list = ds.get_lesson_points_by_stud( studentid )
+        extra_points_list = ds.get_extra_points_by_student( studentid )
 
-        return render_template('points_student.html', studname=name, points_list=points_list)
+        return render_template('points_student.html',
+                               studname=name,
+                               lesson_points_list=lesson_points_list,
+                               extra_points_list=extra_points_list
+                               )
 
 ## --------- points pages -----------------
 # TODO: we want this:
 #       https://stackoverflow.com/questions/31669864/date-in-flask-url
-@app.route('/sessions')
-@app.route('/sessions/<session_date_str>')
-def sessions( session_date_str=None ):
-    if not session_date_str:
-        sessions = ds.get_sessions_list()
-        return render_template('sessions_overview.html', sessions = sessions)
+@app.route('/lessons')
+@app.route('/lessons/<lesson_date_str>')
+def lessons( lesson_date_str=None ):
+    if not lesson_date_str:
+        lessons = ds.get_lessons_list()
+        return render_template('lessons_overview.html', lessons = lessons)
     else:
-        session_date = datetime.strptime(session_date_str, "%Y-%m-%d").date()
-        s = ds.get_session( session_date )
-        sp = ds.get_session_points_by_session( session_date )
-        return render_template('sessions_detailed.html', session=s, points=sp )
+        lesson_date = datetime.strptime(lesson_date_str, "%Y-%m-%d").date()
+        s = ds.get_lesson( lesson_date )
+        sp = ds.get_lesson_points_by_lesson( lesson_date )
+        return render_template('lessons_detailed.html', lesson=s, points=sp )
 
 def init_db( ds ):
     student_count = 10
-    sessions_count = 12
+    lessons_count = 12
+    extra_points_count = 3
 
     for i in range( 0,student_count ):
         ds.add_student( student_record( 'john%04d'%(i, ), "John %d"%(i,) ) )
 
-    for i in range( 0,sessions_count ):
-        ds.add_session( session_record( 'LearningSession%04d'%(i, ), date( 2017, 02, i+1) ) )
+    for i in range( 0,lessons_count ):
+        ds.add_lesson( lesson_record( 'LearningSession%04d'%(i, ), date( 2017, 02, i+1) ) )
 
     for user_i in range( 0,student_count ):
-        for session_i in range( 0,sessions_count ):
-            ds.add_session_points(
-                session_points_record( date( 2017, 02, session_i+1), 'john%04d'%(user_i, ),
+        for lesson_i in range( 0,lessons_count ):
+            ds.add_lesson_points(
+                lesson_points_record( date( 2017, 02, lesson_i+1), 'john%04d'%(user_i, ),
                                         True, False, True ) )
+
+    for user_i in range( student_count ):
+        for ep_i in range( extra_points_count ):
+            ds.add_extra_points(
+                    extra_points_record(
+                        date( 2017, 02, ep_i+1),
+                        student_id='john%04d'%(user_i, ),
+                        points=1,
+                        reason="Some valid reason #%d" % (user_i+ep_i, )
+                        ))
+
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
