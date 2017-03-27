256e6fc300495f84fd8b4becbe086f4823ddcccc
moz <moz@local>
2017-03-27 22:51:25 +0200

    working on utils script to import students

---
+++ src/utils/import_students.py
@@ -0,0 +1,29 @@
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description='read student info from csv file and stores them in database')
+
+    parser.add_argument("csvfile", help="csv to load from")
+    parser.add_argument("-n", "--name",
+                        default=5,
+                        type=int,
+                        help="column containing student name")
+    parser.add_argument("-i", "--id",
+                        default=3,
+                        type=int,
+                        help="column containing student id")
+    parser.add_argument("-d", "--database",
+                        default="../data/gamma.db",
+                        help="database file to use")
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    print "Import student data from %s"% args.csvname
+    print "- Database: %s"% args.database
+    
