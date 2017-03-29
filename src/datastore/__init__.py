a8c64e1391f922a5d0b03a3475ff8c5409b9cf94
moz <moz@local>
2017-03-29 22:35:33 +0200

    added convinience import to datastore

--- src/datastore/__init__.py
+++ src/datastore/__init__.py
@@ -5,3 +5,5 @@ from student import student_record
 from lesson import lesson_record
 from lesson_points import lesson_points_record
 from extra_points import extra_points_record
+
+from sqlalchemy.exc import IntegrityError
